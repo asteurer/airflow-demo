16fd696c137ad29a85e4f07d1c58b1c3059a50fd
Andrew Steurer <andrew.steurer@fermyon.com>
2024-06-14 09:24:23 -0600

    adding config.py

--- dags/example/config.py
+++ dags/example/config.py
@@ -1,15 +1,1 @@
-# S3 and DELTA configs
-s3_endpoint_url = "http://localstack:4566"
-bucket_name = "example-bucket"
-delta_location = "delta_tables"
-database_name = "example_database"
-state_table_name = "state_sales"
-
-# SFTP AND FILE CONFIGS
-SFTP_HOST = "sftp"
-SFTP_PORT = 22
-username = "demo"
-password = "demo"
-
-INPUT_FILE_NAME = "sample_data.xlsx"
-
+INPUT_FILE_NAME = "file.txt"
--- dags/example/put_s3_object.py
+++ dags/example/put_s3_object.py
@@ -2,21 +2,22 @@ from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 from airflow.providers.http.operators.http import HttpOperator
 from airflow.providers.sftp.operators.sftp import SFTPOperator
+from example.config import INPUT_FILE_NAME
 
 @dag(schedule_interval=None, start_date=days_ago(1), catchup=False)
 def put_s3_object():
     get_file = SFTPOperator(
         task_id='get_file',
         ssh_conn_id='ftp_server',
-        remote_filepath='file.txt',
-        local_filepath='/tmp/file.txt',
+        remote_filepath=INPUT_FILE_NAME,
+        local_filepath=f'/tmp/{INPUT_FILE_NAME}',
         create_intermediate_dirs=True,
         operation='get'
     )
 
     @task
     def read_file():
-        with open('/tmp/file.txt', 'rb') as file:
+        with open(f'/tmp/{INPUT_FILE_NAME}', 'rb') as file:
             return file.read().decode('utf-8')
 
     file_content = read_file()
@@ -25,7 +26,7 @@ def put_s3_object():
         task_id='place_in_s3',
         http_conn_id='spin_http',
         method='PUT',
-        headers={'x-uri-path': 'file.txt'},
+        headers={'x-uri-path': INPUT_FILE_NAME},
         data="{{ ti.xcom_pull(task_ids='read_file') }}"
     )
 
