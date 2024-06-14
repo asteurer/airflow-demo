0b81bd3bf7c7354e587a6e10151fd82ddf32c979
Andrew Steurer <andrew.steurer@fermyon.com>
2024-06-14 17:31:24 -0600

    fixing helm/values.yaml and updating comments in install-airflow.sh

--- infra/aks-cluster/install-airflow.sh
+++ infra/aks-cluster/install-airflow.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 kubectl create namespace airflow
 kubectl get namespaces
 
@@ -6,10 +8,15 @@ helm repo update
 helm search repo airflow
 helm install airflow apache-airflow/airflow --namespace airflow --debug
 
+sleep 30
+
 kubectl port-forward svc/airflow-webserver 8080:8080 -n airflow
 
-helm show values apache-airflow/airflow > values.yaml
+# This saves the apachd-airflow helm template locally
+# helm show values apache-airflow/airflow > helm/values.yaml
 
-kubectl create secret generic airflow-ssh-git-secret --from-file=gitSshKey=/your/path/.ssh/id_rsa -n airflow
+# This is for accessing the repo for gitSync via SSH. Currently, the only way gitSync will work is if the repo is public and is being accessed via HTTP. 
+# kubectl create secret generic airflow-ssh-git-secret --from-file=gitSshKey=/home/andyboi/.ssh/github -n airflow
 
-helm upgrade --install airflow apache-airflow/airflow -n airflow -f values.yaml --debug
+# This upgrades the helm install to the changes made in the helm/values.yaml file
+# helm upgrade --install airflow apache-airflow/airflow -n airflow -f helm/values.yaml --debug
